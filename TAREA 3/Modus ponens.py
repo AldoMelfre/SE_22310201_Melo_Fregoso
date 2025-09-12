7d21ca54d08231c1b0f36d0e3ee192c3e18e7f25
Aldo Carlo Melo Fregoso <a22310201@ceti.mx>
2025-09-12 11:52:52 -0600

    Comer

---
+++ TAREA 3/Modus ponens.py
@@ -0,0 +1,24 @@
+# Modus Ponens: Si P entonces Q. P es verdadero, por lo tanto Q es verdadero.
+
+def modus_ponens(P, Q_implicado):
+    """
+    P: premisa (bool)
+    Q_implicado: consecuencia si P es verdadero (bool)
+    Retorna True si se puede inferir Q, False en caso contrario.
+    """
+    if P:
+        return Q_implicado
+    else:
+        return False
+
+# Ejemplo:
+# Si como (P), entonces tengo hambre (Q)
+comer = True
+hambre = True  # Q
+
+resultado = modus_ponens(comer, hambre)
+
+if resultado:
+    print("Por Modus Ponens: Tengo hambre.")
+else:
+    print("No se puede inferir que tengo hambre.")
